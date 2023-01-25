12daec055182cfa523e8b5fc33a74e3dc83a8af5
Matkronh <122084505+Matkronh@users.noreply.github.com>
2023-01-25 13:54:43 +0000

    co

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+const hero = document.getElementById("pokedexx")
+let idStamp = 1
+const idArray = []
 
+async function getApi(url){
+    const response = await fetch (url)
+    const data = await response.json()
+    return data
+}
+
+async function displayPokemonIndex(){
+    
+    const pokemonData = await getApi("https://pokeapi.co/api/v2/pokemon/")
+
+        pokemonData.results.forEach(async pokemon => {
+            idStamp = idStamp + 1
+            const pokemonDetailData = await getApi(pokemon.url)
+            const pokeFrameElement = document.createElement("div")
+            pokeFrameElement.setAttribute("id", pokemon.name)
+            const pokeNameElement = document.createElement("h2")
+            pokeNameElement.textContent = pokemon.name
+            const pokeFramePic = document.createElement("img")
+            pokeFramePic.src = pokemonDetailData.sprites.other["official-artwork"].front_default
+            pokeFrameElement.append(pokeNameElement, pokeFramePic)
+            
+            //const dependency = idArray.findIndex((value) => value.id === pokemon.name)
+            
+            hero.append(pokeFrameElement)
+            idArray.push(pokemon.name)
+            
+    })
+    
+}
+
+
+displayPokemonIndex()
+
+console.log(idArray)
